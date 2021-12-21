cf9f4c9b609c1815673315b8700d61e30736d10e
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-12-21 13:37:26 +0200

    db fix

--- src/Service.DwhServiceBusBridge.DwhDatabase/Migrations/20211221113613_LatTs.Designer.cs
+++ src/Service.DwhServiceBusBridge.DwhDatabase/Migrations/20211221113613_LatTs.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Service.DwhServiceBusBridge.DwhDatabase;
 
@@ -11,9 +12,10 @@ using Service.DwhServiceBusBridge.DwhDatabase;
 namespace Service.DwhServiceBusBridge.DwhDatabase.Migrations
 {
     [DbContext(typeof(DwhContext))]
-    partial class DwhContextModelSnapshot : ModelSnapshot
+    [Migration("20211221113613_LatTs")]
+    partial class LatTs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
